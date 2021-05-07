bbcda5062b59fda9cd6e96f567df0867fbb9aa4d
Boštjan Vižin <bostjan.vizin@crea.si>
2021-05-07 10:48:48 +0200

    Fixed warning.

--- src/Services/SPVChannels/SPVChannels.Test.Functional/TestStartup.cs
+++ src/Services/SPVChannels/SPVChannels.Test.Functional/TestStartup.cs
@@ -27,7 +27,7 @@ namespace SPVChannels.Test.Functional
       Configuration = configuration;
     }
 
-    public IConfiguration Configuration { get; }
+    public new IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public override void ConfigureServices(IServiceCollection services)
